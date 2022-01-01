1033b90b88f4488167df2cea9f7a097355c3a0f8
HarrishArun <95520465+HarrishArun@users.noreply.github.com>
2022-01-01 12:44:39 +0530

    init

---
+++ deal_ud/lib/Screens/producr_detail_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailpage extends StatelessWidget {
+  ProductDetailpage(this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+    );
+  }
+}
